d9e3e411ec48dfb61c5113dde708e01c226a54aa
Dashaheen <gogogugugaga@proton.me>
2024-07-28 15:18:35 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,5 +119,34 @@
 		$('.scrolly').scrolly({
 			speed: 1000
 		});
+	
+	var swiper = new Swiper('.swiper-container', {
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		slidesPerView: 1,
+		spaceBetween: 10,
+		breakpoints: {
+			640: {
+				slidesPerView: 1,
+				spaceBetween: 20,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 40,
+			},
+			1024: {
+				slidesPerView: 3,
+				spaceBetween: 50,
+			},
+		}
+	});
+
 
 })(jQuery);
